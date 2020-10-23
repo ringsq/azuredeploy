dc2a15a9ed2e84a4fceabb21da7c000f168bc0af
Visharad.Dhavle <Visharad.Dhavle@boots.co.uk>
2020-10-23 16:45:38 +0100

    revio: deploy.sh updated to deploy revio

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM mcr.microsoft.com/azure-cli
+RUN apk add gettext
 ADD deploy.sh /bin/
 RUN chmod +x /bin/deploy.sh
 ENTRYPOINT /bin/deploy.sh
-
--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-AZVARS="M5=Magna5"
-for var in ${PLUGIN_ENVVARS//,/ }; do
-    AZVARS=${AZVARS},$var=${!var:-missing}
+# Get PLUGIN_ENVVARS into env.txt so that env sustitution can be done
+echo $PLUGIN_ENVVARS > env.txt
+
+ENV_VARS=$(envsubst < env.txt)
+
+# PLUGIN_ENVVARS has ',' as Internal field separator - IFS
+# This is a most preferred way to split string if source string has spaces.
+IFS=','
+
+# Read array ENV_VARS into ARR
+read -ra ARR <<< "$ENV_VARS"
+
+# create space sperated string
+for i in "${ARR[@]}"; do
+    ENV_VARS_STR="$ENV_VARS_STR $i"
 done
 
+# Store command to be executed in to CREATE, other it adds single quotes to variable
+CREATE="az container create --resource-group ${PLUGIN_RESOURCE_GROUP} --name ${PLUGIN_CONTAINER_NAME} --image ${PLUGIN_IMAGE}:${PLUGIN_IMAGE_VERSION:-latest} --dns-name-label ${PLUGIN_DNS} --ports ${PLUGIN_PORTS:-80} --registry-login-server ${PLUGIN_REGISTRY:-hub.docker.io} --registry-username ${PLUGIN_REGISTRY_USER} --registry-password ${PLUGIN_REGISTRY_PASSWORD} --environment-variables M5=Magna5 ${ENV_VARS_STR}"
+
+# Execute
 az login --service-principal -u ${PLUGIN_AZURE_APPID} -p ${PLUGIN_AZURE_PASSWORD} --tenant ${PLUGIN_AZURE_TENANT} || exit 1
-az container create --resource-group ${PLUGIN_RESOURCE_GROUP} --name ${PLUGIN_CONTAINER_NAME} --image ${PLUGIN_IMAGE}:${PLUGIN_IMAGE_VERSION:-latest} --dns-name-label ${PLUGIN_DNS} --ports ${PLUGIN_PORTS:-80} --registry-login-server ${PLUGIN_REGISTRY:-hub.docker.io} --registry-username ${PLUGIN_REGISTRY_USER} --registry-password ${PLUGIN_REGISTRY_PASSWORD} --environment-variables ${AZVARS//,/ }
+eval $CREATE
