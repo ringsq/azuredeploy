cbdf12972f85d33734e1848b535c020d18b15dde
gaurav-teliax <gaurav.gaglani@teliax.com>
2020-10-26 23:59:52 +0530

    fix: put the envvars args value in double quotes and execute the az container deploy command through eval
    This commit will put the value of the args passed to the --environment-variables in double quotes so that if the value has space in them it is not considered as a seprate variable. The command for the azure container instance will happen using the eval command again for the reason of preserving the space in the --environment variables

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,18 @@
 
 AZVARS="M5=Magna5"
 for var in ${PLUGIN_ENVVARS//,/ }; do
-    AZVARS=${AZVARS},$var=${!var:-missing}
+  AZVARS=${AZVARS},$var="${!var:-missing}"
 done
 
+# Put the env variables in quotes "" to accomodate space in values
+AZVARS=$(echo $AZVARS | sed 's/=\([^,^$]*\)/="\1"/g')
+
+# Print the command being executed
+set -x
+
 az login --service-principal -u ${PLUGIN_AZURE_APPID} -p ${PLUGIN_AZURE_PASSWORD} --tenant ${PLUGIN_AZURE_TENANT} || exit 1
-az container create --resource-group ${PLUGIN_RESOURCE_GROUP} --name ${PLUGIN_CONTAINER_NAME} --image ${PLUGIN_IMAGE}:${PLUGIN_IMAGE_VERSION:-latest} --dns-name-label ${PLUGIN_DNS} --ports ${PLUGIN_PORTS:-80} --registry-login-server ${PLUGIN_REGISTRY:-hub.docker.io} --registry-username ${PLUGIN_REGISTRY_USER} --registry-password ${PLUGIN_REGISTRY_PASSWORD} --environment-variables ${AZVARS//,/ }
+
+CREATE="az container create --resource-group ${PLUGIN_RESOURCE_GROUP} --name ${PLUGIN_CONTAINER_NAME} --image ${PLUGIN_IMAGE}:${PLUGIN_IMAGE_VERSION:-latest} --dns-name-label ${PLUGIN_DNS} --ports ${PLUGIN_PORTS:-80} --registry-login-server ${PLUGIN_REGISTRY:-hub.docker.io} --registry-username ${PLUGIN_REGISTRY_USER} --registry-password ${PLUGIN_REGISTRY_PASSWORD} --environment-variables ${AZVARS//,/ }"
+
+# Execute az container deploy command
+eval $CREATE
